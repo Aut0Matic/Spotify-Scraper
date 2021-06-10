32c90ffcd23b603a5e22410e8b938166302b696f
Aut0Matic <82569441+Aut0Matic@users.noreply.github.com>
2021-06-10 22:21:15 +0930

    Add Musicbrainzngs API
    Images from Spotify were too low resolution (all 640x640). the Musicbrainzngs database contains much higher resolution images to download instead.

--- Spotify-Scraper.py
+++ Spotify-Scraper.py
@@ -1,12 +1,13 @@
 import requests
 import urllib
-
 from requests.api import request
 
-access_token ='BQDmPBjfCIfmz1TrRJvYpULzRz6pq857BxbFosMfyIpAUJl4LHzCtDG3FOID1lZGkIxtq-LfdLGHg7hKxpprazoSh_bEEmpoZdQQkp4B6UaGKNSNsSYoqyJ2nx2Gc33y_96gx0jQZfNWK5I'
-#access_token = input("access token:")
+""" Spotify Request """
 
-#Setting the header and the request url
+access_token ='BQCe22TjrxsNOd0f-Bf9S1TWwxn5GLck9etIaa6SyP2Yfaaq5OeFtjLHPeet7HfF_CI_ej0WlxxeV3Z7Czg8P6OYp0KwNKQ71FF-x_D-tUUgXA9LC0GRMS2m_NjfiP9JL2ecX4YM9aBsXP8'
+# access_token = input("access token:")
+
+# Setting the header and the request url
 headers = {
     "Authorization": f"Bearer {access_token}"
 }
@@ -15,18 +16,42 @@ request_url = f"{endpoint}"
 
 
 # Making a request
-r = requests.get(request_url, headers=headers)
+spotify_request = requests.get(request_url, headers=headers)
 
-json_response = r.json()
-print(json_response)
+# Converting to json
+spotify_json_response = spotify_request.json()
+print(spotify_json_response)
 
-album_name = json_response['item']['album']['name']
-album_id = json_response['item']['album']['id']
-track_name = json_response['item']['name']
+# Retrieving information
+album_name = spotify_json_response['item']['album']['name']
+album_id = spotify_json_response['item']['album']['id']
+track_name = spotify_json_response['item']['name']
 print(track_name)
 print(album_name)
 print(album_id)
 
-album_art_url = json_response['item']['album']['images'][0]['url']
+
+album_art_url = spotify_json_response['item']['album']['images'][0]['url']
 print(album_art_url)
 urllib.request.urlretrieve(album_art_url, "image.jpg")
+
+""" Musicbrainzngs Request """
+
+import musicbrainzngs
+
+mb_endpoint = "http://musicbrainz.org/ws/2/annotation"
+request_url = f"{endpoint}?query=name:{album_name}"
+
+musicbrainzngs_id_request = requests.get(request_url)
+
+musicbrainzngs_json = musicbrainzngs_id_request.json()
+print(musicbrainzngs_json)
+
+musicbrainz_id = musicbrainzngs_json["annotations"]["entity"]
+
+
+
+musicbrainzngs_request = musicbrainzngs.get_image_list(musicbrainz_id)
+musicbrainzngs_json_request = musicbrainzngs_request.json()
+
+print(musicbrainzngs_request['images']['image'])
